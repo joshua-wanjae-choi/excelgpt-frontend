b255c741bb199a2f87082ceee60e04b42edf6f76
joshua-wanjae-choi <joshua.wanjae.choi@gmail.com>
2023-10-18 22:26:57 +0900

    테이블에서 여러개 sheet를 사용하도록 설정.

--- src/component/runner/excel/excel-table.tsx
+++ src/component/runner/excel/excel-table.tsx
@@ -6,27 +6,48 @@ import type IJspreadsheet from "jspreadsheet-ce";
 import "jspreadsheet-ce/dist/jspreadsheet.css";
 
 interface IJspreadsheetWrapper extends HTMLDivElement {
+  // single sheet
   jspreadsheet: IJspreadsheet.JSpreadsheet;
+  // multiple sheets
+  jexcel: IJspreadsheet.JSpreadsheet;
 }
 
 export const ExcelTable = (props: IExcelTable) => {
   const jRef = useRef<IJspreadsheetWrapper>(null);
-  const options: IJspreadsheet.JSpreadsheetOptions = {
-    data: [
-      [1, 2, 4],
-      [3, 6, 9],
-    ],
+  const defaultOption: IJspreadsheet.JSpreadsheetOptions = {
+    about: false,
+    fullscreen: false,
+    defaultColWidth: 50,
     minDimensions: [20, 40],
     rowResize: true,
     tableOverflow: true,
-    tableHeight: "100%",
+    tableHeight: "1000px",
+    tableWidth: "100%",
   };
+  const sheetOptions: IJspreadsheet.TabOptions[] = [
+    {
+      ...defaultOption,
+      sheetName: "sheet1",
+      data: [
+        [1, 2, 4],
+        [3, 6, 9],
+      ],
+    },
+    {
+      ...defaultOption,
+      sheetName: "sheet2",
+      data: [
+        [4, 5, 6],
+        [7, 8, 9],
+      ],
+    },
+  ];
 
   useEffect(() => {
-    if (jRef.current && !jRef.current.jspreadsheet) {
-      jspreadsheet(jRef.current, options);
+    if (jRef.current && !jRef.current.jexcel) {
+      jspreadsheet.tabs(jRef.current, sheetOptions);
     }
-  }, [options]);
+  }, [sheetOptions]);
 
   return <div className={`${styles["excel-table-wrapper"]}`} ref={jRef} />;
 };
